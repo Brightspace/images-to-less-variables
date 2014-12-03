a0826fead6a59b04c5605dafd586cdb2d2c7a55e
Dave Batiste <dave.batiste@d2l.com>
2014-12-03 11:36:11 -0500

    Compressing images before 64-bit encoding them.

--- test/createVariablesSpec.js
+++ test/createVariablesSpec.js
@@ -9,33 +9,33 @@ describe( 'createVariables', function() {
 
 	var imagesPath = path.join( dataPath, '*.png' );
 	var expectedLength = 477;
-	var expectedValue = 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAB3RJTUUH3QwQDjsk/bI5LAAAAAd0RVh0QXV0aG9yAKmuzEgAAAAMdEVYdERlc2NyaXB0aW9uABMJISMAAAAKdEVYdENvcHlyaWdodACsD8w6AAAADnRFWHRDcmVhdGlvbiB0aW1lADX3DwkAAAAJdEVYdFNvZnR3YXJlAF1w/zoAAAALdEVYdERpc2NsYWltZXIAt8C0jwAAAAh0RVh0V2FybmluZwDAG+aHAAAAB3RFWHRTb3VyY2UA9f+D6wAAAAh0RVh0Q29tbWVudAD2zJa/AAAABnRFWHRUaXRsZQCo7tInAAAARVBMVEX///+0ODi0ODi0ODi0ODi1OzvERkbESEjESUneYWHfZGTgaGjhbGzhb2/ic3PjdnbkenrkfX3lgYHmhIXokZHomZnroKDN9k3IAAAABHRSTlMAEICf9IbTdAAAAEdJREFUGFfFz7kRgDAMAMH1UwD9N0kDSCLgGVwB2c1mN6F3kImJNraG2h+gEo0bqhLqhad/gNQXyMPsH4gIxgLENTOhAmLACWvtICC//BPwAAAAAElFTkSuQmCC")';
+	var expectedValueCompressed = 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAARVBMVEX///+0ODi0ODi0ODi0ODi1OzvERkbESEjESUneYWHfZGTgaGjhbGzhb2/ic3PjdnbkenrkfX3lgYHmhIXokZHomZnroKDN9k3IAAAABHRSTlMAEICf9IbTdAAAAEdJREFUGFfFz7kRgDAMAMH1UwD9N0kDSCLgGVwB2c1mN6F3kImJNraG2h+gEo0bqhLqhad/gNQXyMPsH4gIxgLENTOhAmLACWvtICC//BPwAAAAAElFTkSuQmCC")';
 
-	it( 'Should create Less variable for image', function( done ) {
+	it( 'should create Less variable for image', function( done ) {
 
-		imagesToLess( imagesPath ).then( function( variables ) {
+		imagesToLess( imagesPath, { minify: { log: true } } ).then( function( variables ) {
 			expect( variables.length ).not.toBe( 0 );
 			expect( variables[0].name ).toBe( '@icon' );
 			expect( variables[0].length ).toBe( expectedLength );
-			expect( variables[0].value ).toBe( expectedValue );
+			expect( variables[0].value ).toBe( expectedValueCompressed );
 			done();
 		} );
 
 	} );
 
-	it( 'Should prefix the Less variable for image', function( done ) {
+	it( 'should prefix the Less variable for image', function( done ) {
 
 		imagesToLess( imagesPath, { prefix: 'abc-' } ).then( function( variables ) {
 			expect( variables.length ).not.toBe( 0 );
 			expect( variables[0].name ).toBe( '@abc-icon' );
 			expect( variables[0].length ).toBe( expectedLength );
-			expect( variables[0].value ).toBe( expectedValue );
+			expect( variables[0].value ).toBe( expectedValueCompressed );
 			done();
 		} );
 
 	} );
 
-	it( 'Should create a file containing the Less variables if dest option is specified', function( done ) {
+	it( 'should create a file containing the Less variables if dest option is specified', function( done ) {
 
 		var lessPath = path.join( __dirname, 'test.less' );
 		imagesToLess( imagesPath, { dest: lessPath } ).then( function() {
@@ -43,7 +43,7 @@ describe( 'createVariables', function() {
 			var fs = require( 'fs' );
 
 			fs.readFile( lessPath, function( err, contents ) {
-				expect( contents.toString() ).toBe( '@icon: ' + expectedValue + ';\n' );
+				expect( contents.toString() ).toBe( '@icon: ' + expectedValueCompressed + ';\n' );
 				done();
 			} );
 
@@ -51,11 +51,33 @@ describe( 'createVariables', function() {
 
 	} );
 
-	it( 'Should reject on invalid src argument', function( done ) {
+	it( 'should reject on invalid src argument', function( done ) {
 		imagesToLess().catch( function( err ) {
 			expect( err.message ).toBe( 'The src argument is required.' );
 			done();
 		} );
+	} );
+
+	it( 'should log optimization message to console', function( done ) {
+
+		spyOn( console, 'log' );
+
+		imagesToLess( imagesPath, { minify: { log: true } } ).then( function( variables ) {
+			expect( console.log ).toHaveBeenCalled();
+			done();
+		} );
+
+	} );
+
+	it( 'should not log optimization message to console', function( done ) {
+
+		spyOn( console, 'log' );
+
+		imagesToLess( imagesPath, { minify: { log: false } } ).then( function( variables ) {
+			expect( console.log ).not.toHaveBeenCalled();
+			done();
+		} );
+
 	} );
 
 } );
