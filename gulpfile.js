9f349aba146f27a38bff5258b6d6e62903d6c7ef
Devon Powell <Devon.Powell@D2L.com>
2022-06-22 12:21:08 -0400

    Switch to GitHub Actions

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,14 @@
 var coveralls = require('gulp-coveralls'),
 	gulp = require('gulp'),
 	istanbul = require('gulp-istanbul'),
-	jasmine = require('gulp-jasmine'),
-	jshint = require('gulp-jshint');
-
-gulp.task( 'lint', function() {
-	return gulp.src( ['gulpfile.js', 'lib/*.js'] )
-		.pipe( jshint() )
-		.pipe( jshint.reporter('default') );
-} );
+	jasmine = require('gulp-jasmine');
 
 gulp.task( 'coverage', function() {
 	return gulp.src( 'coverage/**/lcov.info' )
 		.pipe( coveralls() );
 } );
 
-gulp.task( 'default', ['lint'], function( cb ) {
+gulp.task( 'default', function( cb ) {
 	gulp.src( ['lib/**/*.js'] )
 		.pipe( istanbul() )
 		.on( 'finish', function() {
